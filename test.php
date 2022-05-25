1182c8e73dda9116f8bdc459562dbd535befe7ac
Puvanan Selvam <puvananselvam@gmail.com>
2022-05-24 20:38:45 -0400

    Fixed the SSL certificate issue.

--- test.php
+++ test.php
@@ -6,15 +6,20 @@
     echo "This be the test page";
     echo "<br>";
 
+
     $url = 'https://www.google.com';
 
     $handle = curl_init();
 
+    //phpinfo();
+
     curl_setopt($handle,CURLOPT_URL,$url);
 
-    //curl_setopt($handle,CURLOPT_SSL_VERIFYPEER,false);
+    //curl_setopt($handle,CURLOPT_SSL_VERIFYPEER,true);
         //SSL certificate verification is turned off in this case to produce result
         //will be focussed on obtaining certificates today.
+
+    //curl_setopt($handle,CURLOPT_CAINFO,"C:\MAMP\conf\php7.4.16\cacert.pem");
     
     $result = curl_exec($handle);
 
