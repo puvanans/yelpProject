a1791934f969387493ec04177856b137e470ae25
Puvanan Selvam <puvananselvam@gmail.com>
2022-05-25 12:09:35 -0400

    Defined variables for the function(yet to be def)

--- test.php
+++ test.php
@@ -1,7 +1,9 @@
 
 <?php 
     
-    //This is test page for trying out newly learned techniques
+    /*
+    
+    This is test page for trying out newly learned techniques
 
     echo "This be the test page";
     echo "<br>";
@@ -11,15 +13,43 @@
     $handle = curl_init();
 
     curl_setopt($handle,CURLOPT_URL,$url);
-
-    //curl_setopt($handle,CURLOPT_SSL_VERIFYPEER,false);
-        //SSL certificate verification is turned off in this case to produce result
-        //will be focussed on obtaining certificates today.
     
     $result = curl_exec($handle);
 
     curl_close($handle);
     
     print_r($result);
+
+    */
+
+    $API_key = "6k3RxTH6fmFnauS3YV8uHGwgzKDadRY7RD6q8KpD1JxUjWD3HlijGm3VVpVGxWLcHPONwcxE44R4ThHkqLVC7qdIecPDXguJYat5E4h_OUmzwo5lGvJl6cUs5x2JYnYx"
+            //unique id provided by the yelp developer account
+            //https://www.yelp.com/developers/v3/manage_app
+
+    assert($API_key,"Please enter in your API key");
+            // This is check if the API key has been entered or not
+
+            
+    //These are the API constants.
+
+    $API_host = "https://api.yelp.com/v3/";
+            //This url is the host of yelp API, accessing any of yelp's Fusion API is through this endpoint
+    
+    $API_business_search = "businesses/search";
+            //This endpoint is used for searching businesses based on keywords, category, location, and price etc.
+
+    $API_business_details = "businesses/"; //Business ID should be added after the slash 
+            //Get rich business data, such as name, address, phone number, photos, Yelp rating, price levels and hours of operation
+
+
+    //Default search terms.
+
+    $default_Term = "Coffee";
+
+    $default_Location = "Philadelphia, PA";
+
+    $search_Limit = 5;
+    
+
 
 ?>
