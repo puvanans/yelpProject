571043400bb4cf78c1e2c4954b55f9e68d05f99e
Puvanan Selvam <puvananselvam@gmail.com>
2022-05-26 20:05:13 -0400

    Devlpmnt halted due to issues in API key provider.

--- test.php
+++ test.php
@@ -40,6 +40,93 @@
 
      return - The JSON response form the response
     */
+
+    function makeRequest($host,$path,$url_params = array()){
+            //This function sends the API call
+         try
+         {
+                 $curl = curl_init();
+
+                 if (false === $curl)
+                        throw new Exception ('Failed to initialize');
+
+
+                 $url = $host.$path."?".http_build_query($url_params);
+
+                //the following method for setting option for th ecurl instacne could be defined individually, and that is what we are going to do.
+
+                 /*
+                 curl_setopt_array($curl,$array(
+                         CURLOPT_URL => $url,
+                         CURLOPT_RETURNTRANSFER => true, //captures response
+                         CURLOPT_ENCODING => "",
+                         CURLOPT_MAXREDIRS => 10,
+                         CURLOPT_TIMEOUT => 30,
+                         CURLOPT_HTTP_VERSION =>CURL_HTTP_VERSION_1_1,
+                         CURLOPT_CUSTOMREQUEST => "GET",
+                         CURLOPT_HTTPHEADER=>array
+                         (
+                                 "authorization:Bearer".$GLOBALS['API_key'],
+                                 "cache-control:no-cache",
+                         ),
+                        ));
+                */
+
+                curl_setopt($curl,CURLOPT_URL,$url);
+                
+                curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+
+                curl_setopt($curl,CURLOPT_ENCODING,"");
+
+                curl_setopt($curl,CURLOPT_MAXREDIRS,10);
+
+                curl_setopt($curl,CURLOPT_TIMEOUT,30);
+
+                curl_setopt($curl,CURLOPT_HTTP_VERSION,CURL_HTTP_VERSION_1_1);
+
+                curl_setopt($curl,CURLOPT_CUSTOMREQUEST,"GET");
+
+                curl_setopt($curl,CURLOPT_HTTPHEADER, array(
+                        "authorization:Bearer".$GLOBALS['API_key'],
+                        "cache-control: no-cache",
+),
+                );
+                
+
+                $response = curl_exec($curl);
+
+                if (false === $response)
+                        throw new Exception(curl_error($curl),curl_errno($curl));
+
+                $http_status = curl_getinfo($curl,CURLINFO_HTTP_CODE);
+                if(200!=$http_status)
+                        throw new Exception($response,$http_status);
+
+                curl_close($curl);
+
+         }
+                catch (Exception $e) 
+                {
+                        trigger_error(sprintf(
+                                'Curl failed with error'.'/n'.' #%d'.'/n'.':%s',
+                                 $e->getCode(),
+                                 $e->getMessage()),
+                                 E_USER_ERROR);
+                } 
+
+         return $response;
+    }
+
+    $host = "https://api.yelp.com/v3/";
+    $path = "businesses/search";
+    $url_params = array(
+        'name'=>'coffee',
+        'location'=>'Philadelphia,PA',
+    );
+
+    makeRequest($host,$path,$url_params);
+
+
     
 
 
